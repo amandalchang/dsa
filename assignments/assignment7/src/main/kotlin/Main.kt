ff301b53bfb116fbcb264f0f3ac28b0ecb99debd
amandalchang <113650953+amandalchang@users.noreply.github.com>
2024-04-02 04:45:59 -0400

    Adding assignment 7 LZ writeup

--- assignments/assignment7/src/main/kotlin/Main.kt
+++ assignments/assignment7/src/main/kotlin/Main.kt
@@ -1,44 +1,8 @@
 package org.example
 
 import LempelZiv
-import Markov
+// import Markov
 import java.io.File
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-//    val filePath = "the-full-bee-movie-script.txt"
-//    try {
-//        val fileContent = File(filePath).readText()
-//        val numSentences = 16
-//        println(Markov(fileContent).generateMarkov(numSentences))
-//    } catch (e: Exception) {
-//        println("An error occurred while reading the file: ${e.message}")
-//    }
-
-    // Repetitive String
-    val input = "ABABABABAABABABABAABABABABA"
-    printCompressionRatio(input)
-
-    // Non-repetitive Sentence
-    val uniqueString = "The quick brown fox jumps over the lazy dog"
-    printCompressionRatio(uniqueString)
-
-    // Non-repetitive String (no spaces)
-    val alphabet = "abcdefgjijklmnopqrstuvwxyz"
-    printCompressionRatio(alphabet)
-
-    // Peter Shor Example
-    val peterShorExample = "AABABBBABAABABBBABBABB"
-    printCompressionRatio(peterShorExample)
-
-    // Bee Movie
-    val beeMoviePath = "the-full-bee-movie-script.txt"
-    val beeMovieString = File(beeMoviePath).readText()
-    printCompressionRatio(beeMovieString)
-}
-
-
 /**
  * Prints the compression ratio between an uncompressed and compressed file;
  * assumes that the compressed file is a bitString while the original input
@@ -47,6 +11,7 @@ fun main() {
  * @param input the original uncompressed string
  * @param debug a boolean which determines whether LempelZiv will print out its
  *              encodings (before translation to binary) for debugging
+ * @return The ratio between the compressed size and the original size
  */
 fun printCompressionRatio(input: String, debug: Boolean = false) {
     val compression = LempelZiv(input, debug).encode()
@@ -57,3 +22,40 @@ fun printCompressionRatio(input: String, debug: Boolean = false) {
         if (debug) println()
     }
 }
+fun main() {
+//    val filePath = "the-full-bee-movie-script.txt"
+//    try {
+//        val fileContent = File(filePath).readText()
+//        val numSentences = 16
+//        println(Markov(fileContent).generateMarkov(numSentences))
+//    } catch (e: Exception) {
+//        println("An error occurred while reading the file: ${e.message}")
+//    }
+
+    // Super Repetitive and Non-unique String
+    val repetitive = "ABABABABAABABABABAABABABABA"
+    printCompressionRatio(repetitive)
+
+    // Repetitive String
+    val repeat = "olin olin olin olin olin olin olin olin olin olin olin"
+    printCompressionRatio(repeat)
+
+    // Non-repetitive Sentence
+    val uniqueSent = "The quick brown fox jumps over the lazy dog"
+    printCompressionRatio(uniqueSent)
+
+    // Maximally Non-repetitive String (no spaces)
+    val alphabet = "abcdefgjijklmnopqrstuvwxyz"
+    printCompressionRatio(alphabet)
+
+    // Peter Shor Example
+    val peterShorExample = "AABABBBABAABABBBABBABB"
+    printCompressionRatio(peterShorExample)
+
+    // The Bee Movie
+    val beeMoviePath = "the-full-bee-movie-script.txt"
+    val beeMovieString = File(beeMoviePath).readText()
+    printCompressionRatio(beeMovieString)
+}
+
+
